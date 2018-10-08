fce49b204af5268e47278a8642e9450182c3a61d
nicholasjovanka <nicholasjovanka@gmail.com>
2018-10-08 20:38:41 +0700

    alien invasion

---
+++ setting.py
@@ -0,0 +1,13 @@
+class Settings():
+    def __init__(self):
+        """Initializing game settings"""
+        self.screen_width = 1600
+        self.screen_height = 900
+        self.bg_color = (255, 255, 255)
+        self.ship_speed_factor=1.5
+        self.bullet_width=3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullet_speed_factor = 1
+        self.bullets_allowed = 3
+
---
+++ ship.py
@@ -0,0 +1,24 @@
+import pygame
+class Ship:
+    def __init__(self,ai_settings,screen):
+        self.screen=screen
+        self.ai_settings=ai_settings
+        self.image=pygame.image.load('IMAGES/ship.bmp')
+        self.rect=self.image.get_rect()
+        self.screen_rect = screen.get_rect()#To treat the screen like its rectangle
+        #Position of the ship
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+        self.moving_right=False
+        self.moving_left=False
+        self.center=float(self.rect.centerx)
+
+    def blitme(self):
+        self.screen.blit(self.image,self.rect)
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.centerx+=self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.rect.centerx-=self.ai_settings.ship_speed_factor
+
